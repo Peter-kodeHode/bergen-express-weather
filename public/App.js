05d717577ba24f13a4be232228f5ec5cd83c8b0d
Peter Daniel Nygaard <peter.kodehode@gmail.com>
2025-09-23 23:06:56 +0200

    Set up express.js project with a frontend and backend setup. Fetching from APIs for weather data and location photos

---
+++ public/App.js
@@ -0,0 +1,46 @@
+
+function fetchWeather() {
+  fetch('/weather') 
+    .then(response => response.json())
+    .then(data => {
+      const weatherTemp = data.properties.timeseries[0].data.instant.details.air_temperature;
+      const weatherHumidity = data.properties.timeseries[0].data.instant.details.relative_humidity;
+      
+      const weatherInfoElement = document.querySelector(".weather-info");
+      weatherInfoElement.innerHTML = `Det er nå <span>${weatherTemp}</span> grader i <span>Bergen</span>,<br> med en luftfuktighet på <span>${weatherHumidity}%</span>`;
+    })
+    .catch(error => {
+      console.error('Error fetching weather:', error);
+    });
+}
+
+
+function fetchImage() {
+  fetch('/image') 
+    .then(response => response.json())
+    .then(data => {
+      const images = data.hits;
+      if (images.length > 0) {
+        const randomIndex = Math.floor(Math.random() * images.length);
+        const randomImage = images[randomIndex];
+        
+        const imageElement = document.querySelector(".image-container img");
+        imageElement.src = randomImage.largeImageURL;
+        imageElement.alt = randomImage.tags;
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching image:', error);
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchWeather();
+  fetchImage();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchWeather();
+  fetchImage();
+});
---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = 3000;
+const apiKey = process.env.API_KEY;
+
+
+app.get("/weather", async (req, res) => {
+  try {
+    const weatherUrl = `https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=60.39&lon=5.32`;
+    const response = await fetch(weatherUrl);
+    const weatherData = await response.json();
+    res.json(weatherData); 
+  } catch (error) {
+    console.error("Error fetching weather:", error);
+    res.status(500).send("Could not get weather data");
+  }
+});
+
+app.get("/image", async (req, res) => {
+  try {
+    const imageUrl = `https://pixabay.com/api/?key=${apiKey}&q=bergen+norway&image_type=photo&orientation=horizontal`;
+    const response = await fetch(imageUrl);
+    const imageData = await response.json();
+    res.json(imageData); 
+  } catch (error) {
+    console.error("Error fetching image:", error);
+    res.status(500).send("Could not get image data");
+  }
+});
+
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
