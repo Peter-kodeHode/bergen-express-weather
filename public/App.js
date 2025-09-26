af5f8a55d36ef01f512bdd4bf40c2ca787c48bab
Peter-kodeHode <peter.kodehode@gmail.com>
2025-09-26 14:42:30 +0200

    Made changes to fetchImage, HTML and CSS to allow for  random Pixabay images of Bergen to be set as background

--- public/App.js
+++ public/App.js
@@ -1,46 +1,49 @@
-const API_URL = window.location.hostname === 'localhost' 
-  ? 'http://localhost:3000' 
-  : 'https://bergen-express-weather-backend.onrender.com';
-
+const API_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "https://bergen-express-weather-backend.onrender.com";
 
 function fetchWeather() {
-  fetch(`${API_URL}/weather`) 
-    .then(response => response.json())
-    .then(data => {
-      const weatherTemp = data.properties.timeseries[0].data.instant.details.air_temperature;
-      const weatherHumidity = data.properties.timeseries[0].data.instant.details.relative_humidity;
-      
+  fetch(`${API_URL}/weather`)
+    .then((response) => response.json())
+    .then((data) => {
+      const weatherTemp =
+        data.properties.timeseries[0].data.instant.details.air_temperature;
+      const weatherHumidity =
+        data.properties.timeseries[0].data.instant.details.relative_humidity;
+
       const weatherInfoElement = document.querySelector(".weather-info");
-      weatherInfoElement.innerHTML = `Det er nå <span>${weatherTemp}</span> grader i <span>Bergen</span>,<br> med en luftfuktighet på <span>${weatherHumidity}%</span>`;
+      weatherInfoElement.innerHTML = `Det er nå <span>${weatherTemp}</span> grader i <span>Bergen</span>, med en luftfuktighet på <span>${weatherHumidity}%</span>`;
     })
-    .catch(error => {
-      console.error('Error fetching weather:', error);
+    .catch((error) => {
+      console.error("Error fetching weather:", error);
     });
 }
 
-
 function fetchImage() {
-  fetch(`${API_URL}/image`) 
-    .then(response => response.json())
-    .then(data => {
+  fetch(`${API_URL}/image`)
+    .then((response) => response.json())
+    .then((data) => {
       const images = data.hits;
       if (images.length > 0) {
         const randomIndex = Math.floor(Math.random() * images.length);
         const randomImage = images[randomIndex];
-        
-        const imageElement = document.querySelector(".image-container img");
-        imageElement.src = randomImage.largeImageURL;
-        imageElement.alt = randomImage.tags;
+
+        // const imageElement = document.querySelector(".image-container img");
+        // imageElement.src = randomImage.largeImageURL;
+        // imageElement.alt = randomImage.tags;
+        const backgroundContainer = document.querySelector(
+          `.background-container`
+        );
+        backgroundContainer.style.backgroundImage = `url(${randomImage.largeImageURL})`;
       }
     })
-    .catch(error => {
-      console.error('Error fetching image:', error);
+    .catch((error) => {
+      console.error("Error fetching image:", error);
     });
 }
 
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   fetchWeather();
   fetchImage();
 });
-
